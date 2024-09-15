a5fb502e72c2400b7ec41c2757f1faa8c3023a1d
Julio Mattos <julio.mattos999@gmail.com>
2024-09-15 12:50:30 -0300

    Create uzumaki.java

---
+++ Exercicios/uzumaki.java
@@ -0,0 +1,16 @@
+package A01;
+
+public class uzumaki extends ninja {
+	
+	boolean temBiju;
+	
+	public void chakaraInfinito() {
+		System.out.print("Você é um Uzumaki e tem Chakara infinito");
+	}
+	
+	@Override
+	public void kunai() {
+		System.out.println("Sou um ninja e posso jogar uma Kunai de AR");
+	}
+
+}
